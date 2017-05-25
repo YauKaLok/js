1c190889d84002241eb6ffae5505109e635b40e0
ayau <arthur.yau@infocast.com.hk>
2017-05-25 12:25:41 +0800

    init

---
+++ src/main/java/com/infocast/js/App.java
@@ -0,0 +1,13 @@
+package com.infocast.js;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
